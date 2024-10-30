70c93a2a362804c13d4542524e1840da43ad2cad
scosman <scosman@users.noreply.github.com>
2024-10-30 19:14:17 -0400

    Tailwind import style change. Node 23 was complaining

--- app/web_ui/tailwind.config.js
+++ app/web_ui/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+import typography from "@tailwindcss/typography"
+
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [typography, daisyui],
   daisyui: {
     themes: [
       {
