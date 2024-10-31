8f605e1872b0a323d7bcf48be7e84528eb5d4680
scosman <scosman@users.noreply.github.com>
2024-10-31 14:52:56 -0400

    Favicons and the like

--- app/web_ui/src/routes/+layout.svelte
+++ app/web_ui/src/routes/+layout.svelte
@@ -67,6 +67,25 @@
 <svelte:head>
   <title>Kiln Studio</title>
   <meta name="description" content="The open source ML product platform" />
+
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="/favicons/apple-touch-icon.png"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/favicons/favicon-32x32.png"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="/favicons/favicon-16x16.png"
+  />
+  <link rel="manifest" href="/favicons/site.webmanifest" />
 </svelte:head>
 
 {#if loading || load_error}
