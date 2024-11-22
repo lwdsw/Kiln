5e42d074082206872ffe60133465a768ec55620d
scosman <scosman@users.noreply.github.com>
2024-11-22 12:51:14 -0500

    Remove unneeded check

--- libs/core/kiln_ai/adapters/fine_tune/__init__.py
+++ libs/core/kiln_ai/adapters/fine_tune/__init__.py
@@ -4,10 +4,6 @@
 A set of classes for fine-tuning models.
 """
 
-import sys
+from . import dataset_split
 
-# Avoid circular import since we use datamodel in some tests
-if "pytest" not in sys.modules:
-    from . import dataset_split
-
-    __all__ = ["dataset_split"]
+__all__ = ["dataset_split"]
