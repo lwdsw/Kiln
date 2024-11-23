18a78edbc11b28b7d9276b2766167b36ac6259d9
scosman <scosman@users.noreply.github.com>
2024-11-23 11:34:26 -0500

    Restore __init__.py

--- libs/core/kiln_ai/adapters/fine_tune/__init__.py
+++ libs/core/kiln_ai/adapters/fine_tune/__init__.py
@@ -4,6 +4,6 @@
 A set of classes for fine-tuning models.
 """
 
-# from . import base_finetune, openai_finetune
+from . import base_finetune, openai_finetune
 
-# __all__ = ["base_finetune", "openai_finetune"]
+__all__ = ["base_finetune", "openai_finetune"]
