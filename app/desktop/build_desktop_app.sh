f5ea49f039e4a647181097933d7d6f5c583ff2b4
Steve Cosman <scosman@users.noreply.github.com>
2025-02-10 00:46:02 -0500

    Build pyinstaller bootloader (#175)
    Build the pyinstaller boot loader ourselves, which decreases anti-virus false positives

--- app/desktop/build_desktop_app.sh
+++ app/desktop/build_desktop_app.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-# move to the root of the project
+# move to the /app directory of the project
 cd "$(dirname "$0")"
 cd ..
+APP_DIR=$PWD
 
 if [[ $* != *--skip-web* ]]; then
   # build the web ui
@@ -12,7 +13,28 @@ if [[ $* != *--skip-web* ]]; then
   cd web_ui
   npm install
   npm run build
-  cd ..
+  cd $APP_DIR
+fi
+
+# Building the bootloader ourselves helps not be falsely detected as malware by antivirus software on windows.
+# We clone pyinstaller, build the bootloader, and install it into the pyproject desktop pyproject.
+if [[ $* == *--build-bootloader* ]]; then
+  echo "Building pyinstaller inlucding bootloader"
+
+  mkdir -p desktop/build/bootloader
+  cd desktop/build/bootloader
+  curl -L https://github.com/pyinstaller/pyinstaller/archive/refs/tags/v6.11.1.tar.gz -o pyinstaller.tar.gz
+  tar -xzf pyinstaller.tar.gz
+  mv pyinstaller-6.11.1 pyinstaller
+  cd pyinstaller/bootloader
+  python ./waf all
+
+  # install the pyinstaller we just built into the desktop pyproject
+  cd $APP_DIR/desktop
+  uv add build/bootloader/pyinstaller
+
+  # return to the /app directory of the project
+  cd $APP_DIR
 fi
 
 mkdir -p desktop/build
@@ -21,22 +43,19 @@ echo "Building for $(uname)"
 if [ "$(uname)" == "Darwin" ]; then
   echo "Building MacOS app"
   cp desktop/mac_taskbar.png desktop/build/taskbar.png
-  cp desktop/mac_icon.png desktop/build/icon.png
   # onedir launches faster, and still looks like 1 file with MacOS .app bundles
-  PLATFORM_OPTS="--onedir --windowed --osx-bundle-identifier=com.kiln-ai.kiln.studio"
+  PLATFORM_OPTS="--onedir --windowed --icon=../mac_icon.png --osx-bundle-identifier=com.kiln-ai.kiln.studio"
 
   PY_PLAT=$(python -c 'import platform; print(platform.machine())')
   echo "Building MacOS app for single platform ($PY_PLAT)"
 elif [[ "$(uname)" =~ ^MINGW64_NT-10.0 ]] || [[ "$(uname)" =~ ^MSYS_NT-10.0 ]]; then
   echo "Building Windows App"
   cp desktop/win_taskbar.png desktop/build/taskbar.png
-  cp desktop/win_icon.png desktop/build/icon.png
-  PLATFORM_OPTS="--windowed --splash=../win_splash.png"
+  PLATFORM_OPTS="--windowed --splash=../win_splash.png --icon=../win_icon.ico"
 elif [ "$(uname)" == "Linux" ]; then
   echo "Building Linux App"
   cp desktop/mac_taskbar.png desktop/build/taskbar.png
-  cp desktop/mac_icon.png desktop/build/icon.png
-  PLATFORM_OPTS="--windowed --onefile --splash=../win_splash.png"
+  PLATFORM_OPTS="--windowed --onefile --splash=../win_splash.png --icon=../mac_icon.png"
 else
   echo "Unsupported operating system: $(uname)"
   exit 1
@@ -44,7 +63,7 @@ fi
 
 # Builds the desktop app
 # TODO: use a spec instead of long winded command line
-pyinstaller $(printf %s "$PLATFORM_OPTS") --icon="./icon.png" \
+pyinstaller $(printf %s "$PLATFORM_OPTS")  \
   --add-data "./taskbar.png:." --add-data "../../web_ui/build:./web_ui/build" \
   --noconfirm --distpath=./desktop/build/dist --workpath=./desktop/build/work \
   -n Kiln --specpath=./desktop/build \
